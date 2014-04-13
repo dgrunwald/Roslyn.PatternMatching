38c4201e351e378343e19824f725ccf29e7abeee
Daniel Grunwald <daniel@danielgrunwald.de>
2014-04-13 16:45:29 +0200

    Create project

---
+++ Roslyn.PatternMatching/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Daniel
+ * Date: 4/13/2014
+ * Time: 16:41
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Roslyn.PatternMatching
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
